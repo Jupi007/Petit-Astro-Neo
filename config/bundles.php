a8c454f1e24e6470864dcd899ff72b5e51be9c8d
Paul Kepinski <paul.kepinski@orange.fr>
2022-12-17 19:17:59 +0100

    Install webpack

--- config/bundles.php
+++ config/bundles.php
@@ -44,4 +44,5 @@ return [
     Sulu\Bundle\ActivityBundle\SuluActivityBundle::class => ['all' => true],
     Sulu\Bundle\TrashBundle\SuluTrashBundle::class => ['all' => true],
     Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
