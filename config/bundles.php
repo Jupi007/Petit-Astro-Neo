08a14691e483886368d80614da5a9a0caf84d0f0
Paul Kepinski <paul.kepinski@orange.fr>
2023-01-04 11:47:27 +0100

    Create bicephal controller

--- config/bundles.php
+++ config/bundles.php
@@ -47,4 +47,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Sulu\Bundle\HeadlessBundle\SuluHeadlessBundle::class => ['all' => true],
 ];
---
+++ src/Controller/Website/BicephalWebsiteController.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Website;
+
+use Sulu\Bundle\HeadlessBundle\Controller\HeadlessWebsiteController;
+use Sulu\Bundle\WebsiteBundle\Controller\DefaultController;
+use Sulu\Component\Content\Compat\PageInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\Service\Attribute\SubscribedService;
+use Symfony\Contracts\Service\ServiceSubscriberInterface;
+use Symfony\Contracts\Service\ServiceSubscriberTrait;
+
+class BicephalWebsiteController extends AbstractController implements ServiceSubscriberInterface
+{
+    use ServiceSubscriberTrait;
+
+    #[SubscribedService]
+    private function defaultController(): DefaultController
+    {
+        return $this->container->get(DefaultController::class);
+    }
+
+    #[SubscribedService]
+    private function headlessController(): HeadlessWebsiteController
+    {
+        /* @return HeadlessWebsiteController */
+        return $this->container->get(HeadlessWebsiteController::class);
+    }
+
+    public function indexAction(
+        Request $request,
+        PageInterface $structure,
+        bool $preview = false,
+        bool $partial = false,
+    ): Response {
+        if ('json' === $request->getRequestFormat()) {
+            return $this->headlessController()->indexAction($request, $structure, $preview, $partial);
+        }
+
+        return $this->defaultController()->indexAction($structure, $preview, $partial);
+    }
+}
