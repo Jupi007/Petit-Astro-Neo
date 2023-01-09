3b41bef9b3407959572858181acfe739587afd9b
Paul Kepinski <paul.kepinski@orange.fr>
2023-01-09 21:45:20 +0100

    Add definitions_selection content type

---
+++ src/Content/DefinitionsSelectionContentType.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Content;
+
+use App\Entity\Definition;
+use App\Repository\DefinitionRepository;
+use Sulu\Component\Content\Compat\PropertyInterface;
+use Sulu\Component\Content\SimpleContentType;
+
+class DefinitionsSelectionContentType extends SimpleContentType
+{
+    public function __construct(
+        private readonly DefinitionRepository $definitionRepository,
+    ) {
+        parent::__construct('definitions_selection', []);
+    }
+
+    /**
+     * @return Definition[]
+     */
+    public function getContentData(PropertyInterface $property): array
+    {
+        $definitions = $this->definitionRepository->findBy([
+            'id' => $property->getValue(),
+        ]);
+
+        foreach ($definitions as $definition) {
+            $definition->setLocale($property->getStructure()->getLanguageCode());
+        }
+
+        return $definitions;
+    }
+
+    public function getViewData(PropertyInterface $property)
+    {
+        return [
+            'ids' => $property->getValue(),
+        ];
+    }
+}
