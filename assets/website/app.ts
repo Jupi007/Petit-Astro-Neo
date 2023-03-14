3922bc0d5b50bac2eaf8be7233031fd87dd8c673
Paul Kepinski <paul.kepinski@orange.fr>
2023-03-14 11:23:58 +0100

    Use alpine focus trap in navigation and popup image

--- assets/website/app.ts
+++ assets/website/app.ts
@@ -1,6 +1,10 @@
+import focus from '@alpinejs/focus';
 import Alpine from 'alpinejs';
+
 import './bootstrap';
 import './styles/app.css';
 
 window.Alpine = Alpine;
+
+Alpine.plugin(focus);
 Alpine.start();
