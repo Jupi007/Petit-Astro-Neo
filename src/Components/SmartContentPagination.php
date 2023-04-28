de993fc1e9f00dec1cff8d97c255ae67afaf6cd5
Paul Kepinski <paul.kepinski@orange.fr>
2023-04-28 16:31:56 +0200

    Create smart-content-pagination twig component

---
+++ src/Components/SmartContentPagination.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Components;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent('smart-content-pagination')]
+class SmartContentPagination
+{
+    /** @var mixed[] */
+    public array $view;
+
+    public string $smartContentId;
+}
