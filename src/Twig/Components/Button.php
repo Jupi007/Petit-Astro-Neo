be12b6887dcea3c808bdb74fe22d540b050e4670
Paul Kepinski <paul.kepinski@orange.fr>
2023-11-11 00:45:00 +0100

    Move twig components traits to common folder

--- src/Twig/Components/Button.php
+++ src/Twig/Components/Button.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Twig\Components;
 
+use App\Twig\Components\Common\ComponentVariantTrait;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
 #[AsTwigComponent('button')]
--- src/Twig/Components/Common/ComponentVariantTrait.php
+++ src/Twig/Components/Common/ComponentVariantTrait.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Twig\Components;
+namespace App\Twig\Components\Common;
 
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
